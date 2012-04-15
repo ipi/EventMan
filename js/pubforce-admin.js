3321205696844832b85ece2ee7cc4401d476500b
Ramon van Belzen <ramoonus@zeelandnet.nl>
2012-04-15 16:34:53 +0200

    removed calendar picture with text (temporary bugfix)

--- js/pubforce-admin.js
+++ js/pubforce-admin.js
@@ -3,8 +3,9 @@ jQuery(document).ready(function($)
 $(".tfdate").datepicker({
     dateFormat: 'D, M d, yy',
     showOn: 'button',
-    buttonImage: 'wp-content/plugins/eventman/images/icon-datepicker.png',
-    buttonImageOnly: true,
+    /*buttonImage: 'wp-content/plugins/eventman/images/icon-datepicker.png',*/
+	buttonText: 'Choose',
+    buttonImageOnly: false,
     numberOfMonths: 3
     });
 });
